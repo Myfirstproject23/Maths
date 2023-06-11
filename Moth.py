9716f4e32cc9ad8a15533a01ce7980116911bbf4
Myfirstproject23 <128229109+Myfirstproject23@users.noreply.github.com>
2023-06-11 17:54:14 +0330

    Add files via upload
    Maths: Adding sides with Python

---
+++ Moth.py
@@ -0,0 +1,29 @@
+import time
+
+number = int(input("Enter you Ribs\n"))
+
+Ribs = 2
+
+answer = number - Ribs
+
+time.sleep(3)
+
+print("Level 1?\n",answer)
+
+Multiplication = 180
+
+answer2 = answer * Multiplication
+
+print("Level2\n",answer2)
+
+Division = int(answer2 /number)
+
+print("answer robots\n",Division)
+
+formol = ("n-2=*180/n")
+
+print("Formol=",formol)
+
+answer3 = answer * Multiplication
+
+print("it You answer bot",answer3)
